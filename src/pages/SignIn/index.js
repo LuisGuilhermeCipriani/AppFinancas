53fafb6262dd825d5442d8e7d186e2e34b02edae
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-08-28 20:05:40 -0300

    Cria página SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Platform } from "react-native";
+import { 
+    Background, 
+    Container, 
+    Logo, 
+    AreaInput, 
+    Input, 
+    SubmitButtom, 
+    SubmitText, 
+    Link, 
+    LinkText 
+} from "./styles";
+
+import { useNavigation } from "@react-navigation/native";
 
 export default function SignIn(){
 
+    const navigation = useNavigation();
+
     return(
-        <View>
-            <Text>Tela Login</Text>
-        </View>
+        <Background>
+            <Container
+                behavior={Platform.OS === 'ios' ? 'padding' : ''} // No Android o teclado sobe o conteúdo automaticamente na tela, 
+                                                                  //mas o IOS não. Por isso é preciso utilizar esse Platform
+                enabled
+            >
+                <Logo
+                    source={require('../../assets/Logo.png')}
+                />
+
+                <AreaInput>
+                    <Input
+                        placeholder="Seu email"
+                    />
+                </AreaInput>
+
+                <AreaInput>
+                    <Input
+                        placeholder="Sua senha"
+                    />
+                </AreaInput>
+
+                <SubmitButtom activeOpacity={0.8}>
+                    <SubmitText>Acessar</SubmitText>
+                </SubmitButtom>
+
+                <Link onPress={() => navigation.navigate('SignUp')}>
+                    <LinkText>Criar uma conta!</LinkText>
+                </Link>
+
+            </Container>
+
+        </Background>
     )
 }
