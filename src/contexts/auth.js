8cd2b940243f601268d1677228397f2151f09282
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-08-30 15:36:38 -0300

    Cria cadastro de usuário

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,5 +1,8 @@
 import React, {createContext, useState} from "react";
 
+import api from '../services/api';
+import { useNavigation } from "@react-navigation/native";
+
 export const AuthContext = createContext({});
 
 function AuthProvider({children}){
@@ -8,8 +11,25 @@ function AuthProvider({children}){
         nome: 'Usuario Teste'
     });
 
+    const navigation = useNavigation();
+
+    async function signUp(email, password, nome){
+        try{
+            const response = await api.post('/users', {
+                name: nome,
+                password: password,
+                email: email,
+            })
+
+            navigation.goBack();
+
+        }catch(err){
+            console.log("ERRO AO CADASTRAR", err);
+        }
+    }
+
     return(
-        <AuthContext.Provider value={{user}}>
+        <AuthContext.Provider value={{user, signUp}}>
             {children}
         </AuthContext.Provider>
     )
