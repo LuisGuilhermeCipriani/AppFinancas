ab7772db3a34ec5c694f89699f4bba7048dd889c
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-09-04 10:33:53 -0300

    Lista gastos

---
+++ src/components/HistoricoList/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Container, TipoText} from './styles';
+
+export default function HistoricoList(){
+
+    return(
+        <Container>
+            <TipoText> Teste Tipo </TipoText>
+        </Container>
+    )
+}
